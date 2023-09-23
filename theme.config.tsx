5d900313cf4c74025797f93cb6ebae22fda10e34
sam bacha <sam@manifoldfinance.com>
2023-09-22 20:51:35 -0700

    fix sidebar

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import { useRouter } from 'next/router';
 import seoConfig from './seo.config';
+import Link from 'next/link'
+import type { NextraThemeLayoutProps } from 'nextra'
+
+export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
+  const { pageMap } = pageOpts
+
 
 const config: DocsThemeConfig = {
   useNextSeoProps() {
@@ -47,7 +53,23 @@ const config: DocsThemeConfig = {
   navigation: {
     prev: true,
     next: true,
-  },
+  }
 };
 
-export default config;
+return (
+  <div>
+    <h1>My Theme</h1>
+    {pageMap.map(item => {
+      if (item.kind === 'MdxPage') {
+        return (
+          <Link key={item.name} href={item.route}>
+            {item.route}
+          </Link>
+        )
+      }
+      return null
+    })}
+    <div style={{ border: '1px solid' }}>{children}</div>
+  </div>
+)
+};
